26cf85acd90953833331b0951e34e55eeb48fbdf
kun <847411844@qq.com>
2021-09-29 21:09:08 +0800

    商品服务-接口定义

---
+++ goods-service/handler/goods.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"context"
+
+	"mall/goods-service/proto"
+)
+
+type GoodsServer struct {
+	//嵌入未实现的接口
+	proto.UnimplementedGoodsServer
+}
+
+//商品接口实现
+func (g *GoodsServer) GoodsList(context.Context, *proto.GoodsFilterRequest) (*proto.GoodsListResponse, error) {
+
+}
+BatchGetGoods(context.Context, *BatchGoodsIdInfo) (*GoodsListResponse, error)
+CreateGoods(context.Context, *CreateGoodsInfo) (*GoodsInfoResponse, error)
+DeleteGoods(context.Context, *DeleteGoodsInfo) (*emptypb.Empty, error)
+UpdateGoods(context.Context, *CreateGoodsInfo) (*emptypb.Empty, error)
+GetGoodsDetail(context.Context, *GoodInfoRequest) (*GoodsInfoResponse, error)
